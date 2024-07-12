851d14a991b93e3de3a8c1b585f7b1ecc99b5cdd
Nishan <nissandhakal11@gmail.com>
2024-07-12 06:26:33 +0545

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="text-3xl font-bold underline bg-red-600 text-2xl  ">
+      my name is amit niraula
+    </div>
+  );
+}
+
+export default App;
---
+++ src/nishan.tsx
@@ -0,0 +1,7 @@
+import "./App.css";
+
+function nishan() {
+  return <div className="text-3xl font-bold underline bg-red-600 text-lg  ">my name is amit niraula</div>;
+}
+
+export default nishan;
